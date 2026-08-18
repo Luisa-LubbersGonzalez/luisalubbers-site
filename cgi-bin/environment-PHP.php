841445268197d3e9d54f95bf1fbc6efc6ce957fd
Luisa Lubbers Gonzalez <llubbersgonzalez@ucsd.edu>
2026-08-17 21:29:47 -0700

    PHP environment?

---
+++ cgi-bin/environment-PHP.php
@@ -0,0 +1,19 @@
+#!/usr/bin/php-cgi
+Header("Cache-Control: no-cache");
+Header("Content-Type: text/html");
+
+echo"<!DOCTYPE html>";"
+echo"<html>";"
+echo"<head>";
+echo"<title>Environment Variables</title>";
+echo"</head>";
+echo"<body>";
+echo"<h1 align=\"center\">Environment Variables</h1>";
+echo"<hr>";
+
+foreach ($_SERVER as $variable => $value) {
+  echo "<b>$variable:</b> $value<br />\n";
+}
+
+echo"</body>";"
+echo"</html>";
